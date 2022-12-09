6f4a8b9b6df5f4e62bb36d14ff3ac651185b436a
vikash05/07/1996 <vikashrajpoot2995@gmail.com>
2022-12-09 15:01:34 +0530

    added test

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
-const bodyParser = require("body-parser");
 const express = require("express");
 var cors = require("cors");
 const app = express();
+const port = 2700;
 
 app.use(cors());
 
-app.get("/", function (req, res) {
-  // res.render("login.html");
-  console.log('logging')
-  res.send("Home");
+app.get("/", function(req, res) {
+    // res.render("login.html");
+    console.log("logging");
+    res.send("Home");
 });
 
+const add = (a, b) => {
+    return a + b;
+};
+module.exports = {
+    add,
+};
+
 app.listen(port, () => {
-  console.log(`Listening on http://localhost:${5001}`);
+    console.log(`Listening on http://localhost:${2700}`);
 });
---
+++ test/index.test.js
@@ -0,0 +1,4 @@
+const { add } = require("../index");
+test("toBe", () => {
+    expect(add(1, 2)).toBe(3);
+});
